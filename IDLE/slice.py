0a6ac20c91d0329bd866236f999f7cd33084ce0d
ruhul <ra.01403288711@gmail.com>
2023-03-30 12:21:56 +0600

    Slice

---
+++ IDLE/slice.py
@@ -0,0 +1,10 @@
+
+numbers = [0,1,2,3,4,5,6,7,8,9,10]
+print(numbers[4:8])
+
+#print from index 5 to the last position!
+
+print(numbers[5:])
+print(numbers[:5])
+print(numbers[:-6])
+print(numbers[::2])
