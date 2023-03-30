4dd27d11e2cbaa3a726cb3c0d272f860995e2da6
ruhul <ra.01403288711@gmail.com>
2023-03-30 11:22:36 +0600

    string , math in python shell programming

---
+++ IDLE/compare.py
@@ -0,0 +1,9 @@
+x=int(input('Enter X'))
+y= int(input('Enter Y'))
+z = int(input('Enter z'))
+
+
+
+result = print(max(x,y,z))
+print('The max Number is: ')
+result
---
+++ IDLE/string.py
@@ -0,0 +1,6 @@
+stringa = 'This is a python string'
+print(len(stringa))
+
+stringB = "Ruhul amin"
+val=10
+print(stringB*val)
