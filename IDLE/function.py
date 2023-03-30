1e6ddab8f12bbf90b46b936a0350957e24dcdf4d
ruhul <ra.01403288711@gmail.com>
2023-03-30 19:08:27 +0600

    Function in python

---
+++ IDLE/function.py
@@ -0,0 +1,16 @@
+
+def fullName(fName,lName):
+    print(fName+' '+ lName)
+
+
+fullName('Ruhul','Amin')
+
+def add(n1,n2):
+    return n1+n2
+
+for i in range(0,20,1):
+    if i%2 is 0:
+        print(add(12,i))
+    else:
+        print("Odd")
+        
