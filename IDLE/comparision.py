bacf044f435da3c49cc3ac1de0e480141c81cdfe
ruhul <ra.01403288711@gmail.com>
2023-03-30 13:38:59 +0600

    Comparision operatior

---
+++ IDLE/comparision.py
@@ -0,0 +1,22 @@
+
+a = 10
+b = 20
+c = 10
+
+if a==b and a==c:print('a b c are equal!')
+# elif a==b or a==c:print('or condition applied!')
+elif a == b and a!=c:print('a and b are equal but c is not')
+elif a==c and a!=b:print('a is equal c')
+else:print('None of them are same!')
+
+string = "My name is Ruhul Amin"
+if 'a' in string:print(True)
+
+p = [1,2,3]
+q = [1,2,3]
+
+
+if p is q:
+    print("They are Same!")
+
+
