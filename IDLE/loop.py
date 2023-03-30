714e4e24180f5b26bd388d1d05089dad480ca6f9
ruhul <ra.01403288711@gmail.com>
2023-03-30 14:07:17 +0600

    loop in python added

---
+++ IDLE/loop.py
@@ -0,0 +1,20 @@
+
+a = 10
+i = 1
+while i<=a: 
+    print(i)
+    i+=1 
+
+sum = 0
+i = 1
+while i<=a:
+    sum+=i 
+    i+=1
+print(sum)
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+for n in numbers:
+    if n%2 is 0:
+        print("Even")
+    else:
+        print("ODD")
