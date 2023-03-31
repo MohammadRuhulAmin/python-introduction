24f549a9e02e55501d1ec669703c8eb10c33faa7
ruhul <ra.01403288711@gmail.com>
2023-03-31 10:38:06 +0600

    Dictionary added

---
+++ IDLE/dictionary.py
@@ -0,0 +1,16 @@
+
+d = {
+    'prevName':'ruhul amin',
+    'name':'sakib',
+    'contact':'122',
+    (1,2,3,4):'touple'
+}
+d['myx'] = 'xxks'
+
+print(d['name'])
+print(len(d))
+print(d[(1,2,3,4)])
+print(d)
+d.keys()
+d.items()
+d.clear()
