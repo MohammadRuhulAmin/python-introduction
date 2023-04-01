6235315473765ec812eb57ae5115924f5d408b1d
ruhul <ra.01403288711@gmail.com>
2023-04-01 23:00:13 +0600

    parametarized constructor

--- ObjectOriented/MyCar.py
+++ ObjectOriented/MyCar.py
@@ -1,10 +1,14 @@
 
 class Car:
-    pass
+    def __init__(self,speed,color):
+        self.speed = speed
+        self.color = color
+        print(speed," => ",color)
+        print("This is a constructor!~it is called first!")
 
-ford = Car()
-audi = Car()
-honda = Car()
+ford = Car(12,'red')
+audi = Car(12,'blus')
+honda = Car(45,'gray')
 
 ford.speed = 150
 audi.speed = 200
@@ -16,3 +20,14 @@ honda.color = 'red'
 
 print(ford.speed)
 print(audi.color)
+
+class Rectangle:
+    def __init__(self,height,width):
+        self.height  = height
+        self.width = width
+
+
+
+rec1 = Rectangle(100,23)
+
+print(rec1.height*rec1.width)
