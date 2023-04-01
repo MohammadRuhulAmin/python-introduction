925c77996abb41201e31b9a795ad457baed27e78
ruhul <ra.01403288711@gmail.com>
2023-04-01 22:44:22 +0600

    basic class in python

---
+++ ObjectOriented/MyCar.py
@@ -0,0 +1,18 @@
+
+class Car:
+    pass
+
+ford = Car()
+audi = Car()
+honda = Car()
+
+ford.speed = 150
+audi.speed = 200
+honda.speed = 133
+
+ford.color = 'blue'
+audi.color = 'Green'
+honda.color = 'red'
+
+print(ford.speed)
+print(audi.color)
